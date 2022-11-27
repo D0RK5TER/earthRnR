a70712ad4a2a527db013877be79bfd90ef7c853a
P. Dexter Assaf <107891735+D0RK5TER@users.noreply.github.com>
2022-11-27 01:05:46 -0800

    render sdjustments

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,6 @@
 // backend/config/database.js
 const config = require('./index');
-const { DEV_DATABASE_HOST, DEV_DATABASE_USERNAME, DEV_DATABASE_PASSWORD } = process.env;
+// const { DEV_DATABASE_HOST, DEV_DATABASE_USERNAME, DEV_DATABASE_PASSWORD } = process.env;
 
 module.exports = {
   development: {
@@ -14,11 +14,14 @@ module.exports = {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     seederStorage: 'sequelize',
-    protocol: 'postgres',
+    // protocol: 'postgres',
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false
+        rejectUnauthorized: false,
+      },
+      define: {
+        schema: process.env.SCHEMA
       }
     }
   }
